89966297a3241f368f2ee2e9304d1303d7d06882
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-14 17:00:01 +0200

    Initial commit

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,8 @@
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        System.out.print("Choose [r]ock, [p]aper or [s]cissors: ");
+    }
+    private static final String ROCK = "Rock";
+    private static final String PAPER = "Paper";
+    private static final String SCISSORS = "Scissors";
+}
